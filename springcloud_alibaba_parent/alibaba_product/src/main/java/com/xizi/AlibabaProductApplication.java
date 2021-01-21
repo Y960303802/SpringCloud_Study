cf480f222a968ccfdf49cce3d4cd2fd0f6f57080
戏子 <58761705+Y960303802@users.noreply.github.com>
2021-01-21 15:15:20 +0800

    SpingCloud_alibaba的总体项目构建结构_没有业务
    SpingCloud_alibaba的总体项目构建结构_没有业务

---
+++ springcloud_alibaba_parent/alibaba_product/src/main/java/com/xizi/AlibabaProductApplication.java
@@ -0,0 +1,13 @@
+package com.xizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaProductApplication.class, args);
+    }
+
+}
---
+++ springcloud_alibaba_parent/alibaba_users/src/main/java/com/xizi/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.xizi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RefreshScope
+public class TestController {
+
+
+    @GetMapping("/user/test")
+    public String test(){
+        return "测试users";
+    }
+}
